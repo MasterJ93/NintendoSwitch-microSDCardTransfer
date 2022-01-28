fcfb15dd2592a1a07defbf725970e96257f56603
Christopher Jr Riley <chrisjr.riley@mountainpaw.com>
2022-01-28 10:16:09 -0500

    Add description

--- swi_sd_transfer.sh
+++ swi_sd_transfer.sh
@@ -1,1 +1,10 @@
 #! /bin/bash
+
+# This script will run a number of commands that prepares the
+# microSD card to insert it to your Nintendo Switch. It removes
+# any files and tweaks flags that may confuse the Switch, resulting
+# in the console to spring issues relating to difficulties in reading
+# the microSD card. The commands used were taken by a post from
+# r/NintendoSwitch:
+# https://www.reddit.com/r/NintendoSwitch/comments/emjvdf/how_to_transfer_sd_card_data_using_macos/
+
